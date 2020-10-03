b398a4ae84cc611eda12edbceb29b009ffce0207
Polina04 <baksheyeva0404@gmail.com>
2020-10-03 23:44:09 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let isChecking = true;
+
+    while (isChecking) {
+        isChecking = false;
+
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            const newStr = str.split(
+                bracketsConfig[i][0] + bracketsConfig[i][1]
+            );
+
+            if (newStr.length > 1) {
+                isChecking = true;
+                str = newStr.join("");
+            }
+        }
+    }
+    return !str;
+};
